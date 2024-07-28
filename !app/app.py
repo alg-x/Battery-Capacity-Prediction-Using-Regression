6b7c470afbc55cf9a118a9a987f166a3774add86
alg-x <algorithmx123@gmail.com>
2024-07-28 12:36:37 +0530

    Updated app.py
    Fixed the app throwing 405 errors whilst hosting. Removed libraries that exceed server hosting limits for available free hosting services and are not required.

--- !app/app.py
+++ !app/app.py
@@ -1,30 +1,30 @@
 import pickle
 import pandas as pd
-from flask import Flask, render_template, request, jsonify
-import os
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+app.template_folder = "./templates"
 
 @app.route("/")
 def main():
-        return render_template("index.html")
+    return render_template("index.html")
 
 @app.route("/prediction", methods=["POST"])
 def prediction():
     if request.method == "POST":
-        cycles = float(request.form['cycles'])
+        cycle = float(request.form['cycle'])
         voltage_battery = float(request.form['voltage_battery'])
         temp_battery = float(request.form['temp_battery'])
         time = float(request.form['time'])
 
-        input_data = {'cycles': [cycles], 'voltage_battery': [voltage_battery], 'temp_battery': [temp_battery],
+        input_data = {'cycle': [cycle], 'voltage_battery': [voltage_battery], 'temp_battery': [temp_battery],
                       'time': [time]}
-        
+
         Xnew = pd.DataFrame(input_data)
-        loaded_model = pickle.load(open('best_model.pkl', 'rb'))
+        loaded_model = pickle.load(open('finalmodel.sav', 'rb'))
         ynew = loaded_model.predict(Xnew)
-    return render_template('result.html', predicted_value=ynew)
-    
+
+        return render_template('result.html', predicted_value=ynew)
+
 if __name__ == "__main__":
     app.run()
-
